c56f763c1b96b99b8df9aa308396826a183b4c2f
Rahul Bhardwaj <kumar.raahul90@gmail.com>
2023-12-08 18:30:49 +0530

    initial commit

---
+++ src/main/java/dummyPipelineProject/dummyPipelineProjectV1/App.java
@@ -0,0 +1,13 @@
+package dummyPipelineProject.dummyPipelineProjectV1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
